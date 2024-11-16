d8651364759da0b177b447b674af9a535927a96e
ALEX KEMBOI <AKEMBOI@fintech-group.com>
2024-11-16 07:15:31 +0300

    build successfully

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { useEffect } from "react";
 import Image from "next/image";
-import alex from '@/images/ikonex.png';
-import capture from '@/images/capture.png';
-import certs from '@/images/certs.jpg';
-import afrecommerce from '@/images/afrecommerce.jpg';
+import alex from '../images/ikonex.jpg';
+import capture from '../images/capture.png';
+import certs from '../images/certs.jpg';
+import afrecommerce from '../images/afrecommerce.jpg';
 
 const Portfolio = () => {
 	const portfolioData = [

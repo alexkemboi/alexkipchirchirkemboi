c0c4dc6360236b97a0ed0bc5f960a9c754144b6b
ALEX KEMBOI <AKEMBOI@fintech-group.com>
2024-11-18 17:25:35 +0300

    changed the endpoint

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -19,7 +19,7 @@ const Contact = () => {
 		e.preventDefault();
 
 		try {
-			const response = await fetch("https://dainty-druid-2763c3.netlify.app/api", {
+			const response = await fetch("https://kemboialex.netlify.app/api", {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
